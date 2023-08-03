6b318cd57f30c33422669407229154c095578f4c
ymkdev <97513263+ymkdev@users.noreply.github.com>
2023-08-03 13:36:57 +0900

    Update hintScriptto.cs

--- Assets/Scripts/dangdang_script/hintScriptto.cs
+++ Assets/Scripts/dangdang_script/hintScriptto.cs
@@ -19,7 +19,7 @@ public class hintScriptto : MonoBehaviour
     void gotoAnotherScene()
     {
         //if (Input.GetMouseButtonDown(0))
-        SceneManager.LoadScene(6);
+        SceneManager.LoadScene("dialoggue 1");
     }
 
     // Update is called once per frame
