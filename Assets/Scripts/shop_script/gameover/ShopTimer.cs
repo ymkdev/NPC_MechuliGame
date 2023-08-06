68845d1ff1b76464679fe8645fd82749b2525cb5
SoJeong <105548648+ozoqzm@users.noreply.github.com>
2023-08-06 15:24:17 +0900

    shopping게임 타이머수정, 시작화면 버튼 수정, 빌드세팅

--- Assets/Scripts/shop_script/gameover/ShopTimer.cs
+++ Assets/Scripts/shop_script/gameover/ShopTimer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class shop_Timer : MonoBehaviour
+public class ShopTimer : MonoBehaviour
 {
     [SerializeField] GameObject fail;
     [SerializeField] GameObject success;
