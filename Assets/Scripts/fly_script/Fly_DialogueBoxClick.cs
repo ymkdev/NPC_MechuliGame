208bc6bf1266d019ad19b6ecc7909e88864a3a96
yjin-jo <dpwls5100@icloud.com>
2023-08-07 23:05:46 +0900

    [update] 파리 잡기 게임 수정
    1. 클리어 후 메인맵 이동
    Fly_DialogueManager.cs -> NextSentence() 함수 else 부분 (114번 line)
    2. 시나리오 박스 화살표 삭제

--- Assets/Scripts/fly_script/Fly_DialogueBoxClick.cs
+++ Assets/Scripts/fly_script/Fly_DialogueBoxClick.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class Fly_ArrowClick : MonoBehaviour, IPointerDownHandler
+public class Fly_DialogueBoxClick : MonoBehaviour, IPointerDownHandler
 {
     public void OnPointerDown(PointerEventData eventData) // 터치시
     {
-        if (!Fly_DialogueManager.instance.isTyping) //타이핑 효과 중엔 클릭 x
+        /*if (!Fly_DialogueManager.instance.isTyping) //타이핑 효과 중엔 클릭 x
         {
             if(Fly_DialogueManager.instance.getHint)
 
             Debug.Log("다음 대사 호출");
             Fly_DialogueManager.instance.NextSentence();
-        }
+        }*/
+        Fly_DialogueManager.instance.NextSentence();
+
     }
 }
